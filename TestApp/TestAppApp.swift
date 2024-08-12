a13409495dc21d96636fe448ed583dc199fd562f
LachinFernando <lachin.fernando@pyxeda.ai>
2024-08-12 07:57:45 +0530

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Lachin Fernando on 2024-08-12.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
